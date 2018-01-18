bc9dbb776f91b67e4ebf70bda91b2048dd0c0ae2
Yevgeny Kazakov <yevgeny.kazakov@uni-ulm.de>
2018-01-18 22:47:56 +0100

    Mavenize docker build and run
    to compile and start the docker one can use:
    
    mvn clean install docker:build docker:start

---
+++ src/main/docker/pinpointing/workspace/experiments/resolution.TOP_DOWN.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TIMEOUT=$1
+shift
+GLOBAL_TIMEOUT=$1
+shift
+QUERY_FILE=$1
+shift
+ENCODING_DIR=$1
+shift
+SCRIPTS_DIR=$1
+shift
+OUTPUT_DIR=$1
+shift
+
+SELECTION=TOP_DOWN
+
+java -Xmx7G -Xms2G -cp "$CLASSPATH" org.liveontologies.pinpointing.RunJustificationExperiments -t "$TIMEOUT"000 -g "$GLOBAL_TIMEOUT"000 $OUTPUT_DIR/record.csv $QUERY_FILE org.liveontologies.pinpointing.experiments.SatResolutionJustificationExperiment -- $ENCODING_DIR $SELECTION 2>&1 > $OUTPUT_DIR/out.log | tee $OUTPUT_DIR/err.log 1>&2
+
